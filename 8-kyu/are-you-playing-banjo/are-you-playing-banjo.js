89e44f2179c77959c9d4a5577e39ac0643f9aacc
Cristina Maria Leascu <124153827+CristinaLeascu@users.noreply.github.com>
2025-03-12 23:55:09 +0000

    Add solution - CodeHub

---
+++ 8-kyu/are-you-playing-banjo/are-you-playing-banjo.js
@@ -0,0 +1,9 @@
+function areYouPlayingBanjo(name) {
+  if (name[0].toLowerCase() === "r" || name[0].toLowerCase() === "R" ) {
+  return name + " plays banjo";
+    } else {
+  return name + " does not play banjo";
+    }
+}
+​
+​
